ace0aeebfe2acf49c6a79657784eeeba9aac476f
san123desh <suuchang53@gmail.com>
2022-04-03 19:49:01 +0545

    datatypes added inside class

---
+++ classes/datatypes.js
@@ -0,0 +1,36 @@
+// console.log('Hello world');
+// console.error('not good');
+// var age = 21;
+// age = "s";
+
+//Datatypes ->8
+//Numbers-FP
+//var population = 325124;
+//var bigNum = 3456787654345678765434567654345673456;
+//console.log(1/0);
+//console.log("s"/0);
+//console.log(bigNum);
+
+//String-sequence of char
+var name = "Sandesh shrestha";//double quoite and single quote are valid.
+var name1 = 'SANDESH SHRESTHA';
+console.log(name1);
+console.log('My name is ${name}');
+
+//Boolean
+var isAnimal = true;
+
+//undefined
+var year;
+console.log(year);
+
+//null
+var u=null;
+
+//object
+//symbol
+//typeof
+console.log(typeof name);
+console.log(typeof 45n);
+console.log(typeof Math);
+console.log(typeof null);
