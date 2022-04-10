b40308d509829334f9f3c77284e02e713304d5b8
san123desh <suuchang53@gmail.com>
2022-04-10 18:00:41 +0545

    loop in js

---
+++ classes/bmi.js
@@ -0,0 +1,13 @@
+let pm = 62;
+let ph = 5.8;
+let Rm = 60;
+let Rh = 5.9;
+
+var bmip = pm/ph **2;
+var bmir = Rm/Rh **2;
+console.log(bmip);
+console.log(bmir);
+
+var prabinHiherBMI = bmip>bmir === true;
+console.log(prabinHiherBMI);
+
---
+++ classes/lesson5-StringandTemplateLetrals.js
@@ -0,0 +1,18 @@
+const a = "kriteeka";
+const b = "Bikal";
+const c = new String('Kriteeka and Bikal..');
+
+console.log(a.length);
+console.log(a.charAt(1));
+console.log(a[1]);
+console.log(a == b);
+
+let para = "ont \
+new line";
+
+para = `on new
+line`//template letrals using backtick
+
+//string interpolation
+console.log(`My collegeis ${a+b+c}`);
+
